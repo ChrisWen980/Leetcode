73f195131cd2adb5c46bd970ba860e143c0f1aaa
ChrisWen980 <chriswen980@gmail.com>
2026-01-21 21:20:00 -0700

    getting back into it

---
+++ Python/11_container_with_most_water_2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        L, R, largestArea = 0, len(height)-1, 0
+        
+        while L < R:
+            largestArea = max(largestArea, min(height[L], height[R]) * (R - L))
+            
+            if height[L] < height[R]:
+                L += 1
+            elif height[R] < height[L]:
+                R -= 1
+            else:
+                L += 1
+                R -= 1
+        return largestArea
+
+sol = Solution()
+ans = sol.maxArea([1,8,6,2,5,4,8,3,7])
+print(f'This is the ans: {ans}')
